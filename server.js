9ca1c5894e103ce79e9dec0c368b8d95763e938f
Till <tillsimonmichels@gmail.com>
2021-07-12 16:37:05 +0200

    Add study files

--- server.js
+++ server.js
@@ -22,6 +22,21 @@ app.get('/x3dom', function(req, res) {
 app.get('/hapd3cation', function(req, res) {
     res.sendFile(path.join(__dirname, '/hapd3cation.html'));
 });
+app.get('/studie01', function(req, res) {
+    res.sendFile(path.join(__dirname, '/study_data_01.html'));
+});
+app.get('/studie02', function(req, res) {
+    res.sendFile(path.join(__dirname, '/study_data_02.html'));
+});
+app.get('/studiendaten1', function(req, res) {
+    res.sendFile(path.join(__dirname, '/study_data_03.html'));
+});
+app.get('/studiendaten2', function(req, res) {
+    res.sendFile(path.join(__dirname, '/study_data_04.html'));
+});
+app.get('/beschreibung', function(req, res) {
+    res.sendFile(path.join(__dirname, '/study_description.html'));
+});
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
