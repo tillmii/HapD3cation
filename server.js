3161e08a11fe4c06682f1f4dd2e302f505d7d6ed
Till <tillsimonmichels@gmail.com>
2021-07-22 21:46:26 +0200

    Restructure project

--- server.js
+++ server.js
@@ -4,38 +4,42 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use('/static', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/OpenJSCAD.org', express.static(__dirname + '/OpenJSCAD.org'));
 
 // sendFile will go here
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/lib/html/index.html'));
 });
 app.get('/d3_example', function(req, res) {
-    res.sendFile(path.join(__dirname, '/d3_example.html'));
+    res.sendFile(path.join(__dirname, '/lib/html/d3_example.html'));
 });
 app.get('/vega_example', function(req, res) {
-    res.sendFile(path.join(__dirname, '/vega_example.html'));
+    res.sendFile(path.join(__dirname, '/lib/html/vega_example.html'));
 });
 app.get('/x3dom', function(req, res) {
-    res.sendFile(path.join(__dirname, '/HelloX3DOM.html'));
+    res.sendFile(path.join(__dirname, '/lib/html/HelloX3DOM.html'));
 });
 app.get('/hapd3cation', function(req, res) {
-    res.sendFile(path.join(__dirname, '/hapd3cation.html'));
+    res.sendFile(path.join(__dirname, '/lib/html/hapd3cation.html'));
 });
 app.get('/studie01', function(req, res) {
-    res.sendFile(path.join(__dirname, '/study_data_01.html'));
+    res.sendFile(path.join(__dirname, '/lib/html/study_data_01.html'));
 });
 app.get('/studie02', function(req, res) {
-    res.sendFile(path.join(__dirname, '/study_data_02.html'));
+    res.sendFile(path.join(__dirname, '/lib/html/study_data_02.html'));
 });
 app.get('/studiendaten1', function(req, res) {
-    res.sendFile(path.join(__dirname, '/study_data_03.html'));
+    res.sendFile(path.join(__dirname, '/lib/html/study_data_03.html'));
 });
 app.get('/studiendaten2', function(req, res) {
-    res.sendFile(path.join(__dirname, '/study_data_04.html'));
+    res.sendFile(path.join(__dirname, '/lib/html/study_data_04.html'));
 });
 app.get('/beschreibung', function(req, res) {
-    res.sendFile(path.join(__dirname, '/study_description.html'));
+    res.sendFile(path.join(__dirname, '/lib/html/study_description.html'));
+});
+app.get('/jscad', function(req, res) {
+    res.sendFile(path.join(__dirname, '/lib/html/jscad_hapd3cation.html'));
 });
 
 app.listen(port);
