5d45f96ccba0a989d6943d0a482153a523c75897
Till <tillsimonmichels@gmail.com>
2021-08-09 14:48:25 +0200

    Add logo and example specification

---
+++ src/compiler.js
@@ -0,0 +1,11 @@
+function compile(specification) {
+
+    try {
+        let jsonSpec = JSON.parse(specification)
+    } catch(e) {
+        return false;
+    }
+
+
+
+}
--- src/index.js
+++ src/index.js
@@ -18,9 +18,12 @@ import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from 'react-bootstrap/Button';
 import Image from "react-bootstrap/Image";
+import Table from 'react-bootstrap/Table'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlay } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+
+import { CsvToHtmlTable } from 'react-csv-to-table';
 
 
 class NavbarComponent extends React.Component {
@@ -38,13 +41,13 @@ class NavbarComponent extends React.Component {
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Button variant="outline-success">
-                                <FontAwesomeIcon icon={faPlay} />
-                            </Button>
+                        <Nav className="ms-auto" >
                             <NavDropdown title="Examples" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="#barchart">Barchart</NavDropdown.Item>
                             </NavDropdown>
+                            <Button variant="outline-success">
+                                <FontAwesomeIcon icon={faPlay} />
+                            </Button>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
@@ -73,11 +76,12 @@ class EditorComponent extends React.Component {
     render() {
         const code = this.state.code;
         const options = {
-            language: "javascript",
+            language: "json",
             minimap: {
                 enabled: false
             },
             automaticLayout: true,
+            wordWrap: "on",
         };
         return (
             <MonacoEditor
@@ -95,8 +99,12 @@ class EditorComponent extends React.Component {
 class DataComponent extends React.Component {
     render() {
         return (
-            <div>
-                {this.props.specification}
+            <div className="scrollable">
+                    <CsvToHtmlTable
+                        data={JSON.parse(this.props.specification).data.values}
+                        csvDelimiter=","
+                        tableClassName="table table-striped table-bordered table-hover"
+                    />
             </div>
         );
     }
@@ -106,7 +114,22 @@ class Game extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            specification: 'Test',
+            specification: '{\n'+
+                '    "description": "Das folgende Diagramm enthält alle Impfungen bis zum einschließlich 07. Juli 2021 aus 8 verschiedenen Bundesländern. Die Säulen in der hinteren Reihe (1) enthalten die Zahlen aller mindestens einmal geimpften Personen. Die Säulen in der vorderen Reihe (2) enthalten die Zahlen aller vollständig geimpften Personen. Die Höhe jeder Säule repräsentiert die Anzahl der Menschen, die geimpft wurden. Weiterhin ist jede Säule in 2 Abschnitte unterteilt: Der untere Abschnitt ist weniger rau und repräsentiert alle geimpften Personen unter 60 Jahren, der ober Abschnitt ist stärker angeraut und repräsentiert alle geimpften Personen, welche 60 Jahre oder älter sind. Die Bundesländer treten von links nach rechts in folgender Reihenfolge auf: Berlin (BE), Brandenburg (BB), Hessen (HE), Rheinland-Pfalz (RP), Sachsen (SN), Sachsen-Anhalt (ST), Schleswig-Holstein (SH), Thüringen (TH). An den Seiten jeder Säule befinden sich Markierungen. Diese haben jeweils einen Abstand von einem Zentimeter. Jeder Abschnitt repräsentiert ca. 350000 Personen.",\n'+
+                '    "data": {\n'+
+                '        "values": "Bundesland,Alter,Anzahl,Impfstatus\\nBE,bis_59,1249009,E\\nBB,bis_59,646375,E\\nHE,bis_59,2014537,E\\nRP,bis_59,1279323,E\\nSN,bis_59,952460,E\\nST,bis_59,534312,E\\nSH,bis_59,973072,E\\nTH,bis_59,539266,E\\nBE,ab_60,770509,E\\nBB,ab_60,636215,E\\nHE,ab_60,1436865,E\\nRP,ab_60,1032644,E\\nSN,ab_60,992991,E\\nST,ab_60,600911,E\\nSH,ab_60,753526,E\\nTH,ab_60,563912,E\\nBE,bis_59,772856,V\\nBB,bis_59,414875,V\\nHE,bis_59,1386409,V\\nRP,bis_59,822881,V\\nSN,bis_59,719440,V\\nST,bis_59,356159,V\\nSH,bis_59,614200,V\\nTH,bis_59,365282,V\\nBE,ab_60,674991,V\\nBB,ab_60,518952,V\\nHE,ab_60,1095244,V\\nRP,ab_60,805295,V\\nSN,ab_60,849376,V\\nST,ab_60,504089,V\\nSH,ab_60,615654,V\\nTH,ab_60,460187,V\\n",\n'+
+                '        "format": {\n'+
+                '            "type": "csv"\n'+
+                '        }\n'+
+                '    },\n'+
+                '    "mark": "bar",\n'+
+                '    "encoding": {\n'+
+                '        "x": {"field": "Bundesland", "type": "nominal"},\n'+
+                '        "y": {"field": "Impfstatus", "type": "nominal"},\n'+
+                '        "z": {"field": "Anzahl", "type": "quantitative"},\n'+
+                '        "roughness": {"field": "Alter", "type": "ordinal"}\n'+
+                '    }\n'+
+                '}'
         }
     }
 
@@ -126,7 +149,7 @@ class Game extends React.Component {
             <div className="full-height">
                 <NavbarComponent />
                 <Split
-                    className="split"
+                    className="split full-height-minus-navbar"
                 >
                     <div
                         className="full-height"
